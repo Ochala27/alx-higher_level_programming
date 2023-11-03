3cb1580d66ee5ca98d232cb42e9b8a68544fb51b
Ochala27 <ochalaonuche3@gmail.com>
2023-11-03 01:06:02 -0700

    txt

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A Locked Class Schema"""
+
+
+class LockedClass:
+    """Locked Class Class"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init class method nothing much"""
+        pass
